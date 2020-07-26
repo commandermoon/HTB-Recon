fee5f9b5b0367575868c600fc103ae90047957f1
commandermoon <46551775+commandermoon@users.noreply.github.com>
2020-07-26 18:10:37 -0500

    Update htb-recon.sh

--- htb-recon.sh
+++ htb-recon.sh
@@ -35,10 +35,21 @@ then
   fi
 
   ## Running Commands
-  nmap -sC -sV -oN recon/initial_nmap.txt -v -Pn $target 
-  sudo masscan -p 1-65535,U:1-65535 -e $interface -oL recon/allports.txt --rate=1000 -vv -Pn $target
+  echo "[*] Starting scans on interface $interface"
+  sudo nmap $target -p- -T4 -Pn -sS | grep "open" | cut -d "/" -f 1 > ~/HackSpace/$name/recon/openports.txt
+  num_ports=$(cat ~/HackSpace/$name/recon/openports.txt | wc -l)
+  echo "[*] $num_ports open tcp ports detected. Scanning now" 
+  ports=$(cat ~/HackSpace/$name/recon/openports.txt)
+  ports=$(echo $ports | sed -e "s/ /,/g")
+  echo "[*] Starting aggressive nmap on $target"
+  nmap $target -p $ports -A -T4 -oN recon/initial_nmap.txt -Pn > ~/HackSpace/$name/recon/fullnmap.txt
+  echo "[*] Nmap complete. Time to bust"
   gobuster dir -u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dirscan.txt
-  ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$target/FUZZ | tee recon/ffuf.txt 
+  echo "[*] Bust finished. It's fuzz o' clock"
+  ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$target/FUZZ | tee recon/ffuf.txt
+  echo "[*] It's getting late. Time for mass"
+  sudo masscan -p 1-65535 -e $interface -oL ~/HackSpace/$name/recon/allports.txt --rate=1000 -Pn $target > ~/HackSpace/$name/recon/massscan.txt   
+  echo "[*] Mass is over. Go in peace"
 
   echo "# Info" >> Notes.md 
   echo "* Name: $name" >> Notes.md 
